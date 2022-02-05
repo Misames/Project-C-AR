3752fd2cc45713fd6d05c1e5f63edf4769fc1551
François Bouscaillou <fbouscaillou@myges.fr>
2022-02-05 16:53:57 +0100

    add timer + pause button

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,54 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
+    private float timer;
+    private bool isPause;
+    private Dictionary<int, GameObject> listPlayer = new Dictionary<int, GameObject>();
+    [SerializeField] private TextMeshProUGUI timerUI;
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject localRecord;
+    [SerializeField] private GameObject liveRanking;
 
-    // Start is called before the first frame update
+
     void Start()
     {
+        Debug.Log("scene start !");
     }
 
-    // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPause) Resume();
+            else Pause();
+        }
+
+        timerUI.text = timer.ToString();
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        pauseMenu.SetActive(true);
+        isPause = true;
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+        isPause = false;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1;
     }
 }
