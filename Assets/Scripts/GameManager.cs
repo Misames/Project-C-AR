b0dc3587387619983ed9dd20acca81051a30ad4f
Raphael <r.vejus@gmail.com>
2022-02-26 15:28:05 +0100

    Ajout Start Game avec element qui force le joueur a se deplacer pour voir le depart de la course

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,41 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Net;
+using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    public GameObject feu;
     // Start is called before the first frame update
     void Start()
     {
+        StopGame();
     }
 
     // Update is called once per frame
     void Update()
     {
     }
+
+    void StopGame()
+    {
+        GameObject Car;
+        Car= GameObject.FindGameObjectWithTag("CarPlayer");
+        Car.GetComponent<CarController>().enabled = false;
+        StartCoroutine(Decompte());
+    }
+
+    IEnumerator Decompte()
+    {
+        yield return new WaitForSeconds(3);
+        StartGame();
+    }
+    
+    void StartGame()
+    {
+        GameObject Car;
+        Car= GameObject.FindGameObjectWithTag("CarPlayer");
+        Car.GetComponent<CarController>().enabled = true;
+    }
+    
 }
