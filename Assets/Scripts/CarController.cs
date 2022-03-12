97c2ba6173907cf6bffd2745f988479071005405
François Bouscaillou <fbouscaillou@myges.fr>
2022-03-12 19:31:59 +0100

    format code

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -2,19 +2,19 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    public Rigidbody _Rigidbody;
+    public Rigidbody rigidBody;
     public float forwardAcceleration = 7f;
-    public float BackAcceleration = 3f;
+    public float backAcceleration = 3f;
     public float maxSpeed = 40f;
     public float turnStrenght = 180f;
     public float gravityforce = 15f;
-    public float DragOnGround = 3f; 
-    
-    private float SpeedInput;
-    private float TurnInput;
+    public float dragOnGround = 3f;
+
+    private float speedInput;
+    private float turnInput;
     private bool isGrounded;
 
-    public LayerMask Ground;
+    public LayerMask ground;
     public float GroundRayLenght = 0.5f;
     public Transform groundRayPoint1;
 
@@ -22,45 +22,42 @@ public class CarController : MonoBehaviour
     public Transform rightFrontWheel;
     public Transform leftBackWheel;
     public Transform rightBackWheel;
-    public float MaxWheelTurn = 25f;
+    public float maxWheelTurn = 25f;
 
-    void Start()
+    private void Start()
     {
-        _Rigidbody.transform.parent = null;
+        rigidBody.transform.parent = null;
     }
 
-    void Update()
+    private void Update()
     {
-        SpeedInput = 0f;
+        speedInput = 0f;
         if (Input.GetAxis("Vertical") > 0)
+            speedInput = Input.GetAxis("Vertical") * forwardAcceleration * 1000f;
+        else if (Input.GetAxis("Vertical") < 0)
         {
-            SpeedInput = Input.GetAxis("Vertical") * forwardAcceleration  * 1000f;
-        }else if (Input.GetAxis("Vertical") < 0)
-        {
-            SpeedInput = Input.GetAxis("Vertical") * BackAcceleration* 1000f;
-            if (SpeedInput < 0)
-            {
-                SpeedInput = 0;
-            }
+            speedInput = Input.GetAxis("Vertical") * backAcceleration * 1000f;
+            if (speedInput < 0)
+                speedInput = 0;
         }
 
         if (isGrounded)
         {
-            TurnInput = Input.GetAxis("Horizontal");
-        
+            turnInput = Input.GetAxis("Horizontal");
             transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f,
-                TurnInput * turnStrenght * Time.deltaTime, 0f));
+                turnInput * turnStrenght * Time.deltaTime, 0f));
         }
-        leftFrontWheel.localRotation = Quaternion.Euler(leftFrontWheel.localRotation.eulerAngles.x,(TurnInput*MaxWheelTurn),leftFrontWheel.localRotation.eulerAngles.z);
-        rightFrontWheel.localRotation = Quaternion.Euler(rightFrontWheel.localRotation.eulerAngles.x,(TurnInput*MaxWheelTurn),rightFrontWheel.localRotation.eulerAngles.z);
-        transform.position = _Rigidbody.transform.position;
+
+        leftFrontWheel.localRotation = Quaternion.Euler(leftFrontWheel.localRotation.eulerAngles.x, (turnInput * maxWheelTurn), leftFrontWheel.localRotation.eulerAngles.z);
+        rightFrontWheel.localRotation = Quaternion.Euler(rightFrontWheel.localRotation.eulerAngles.x, (turnInput * maxWheelTurn), rightFrontWheel.localRotation.eulerAngles.z);
+        transform.position = rigidBody.transform.position;
     }
 
     private void FixedUpdate()
     {
         isGrounded = false;
         RaycastHit hit;
-        if (Physics.Raycast(groundRayPoint1.position, -transform.up, out hit, GroundRayLenght, Ground))
+        if (Physics.Raycast(groundRayPoint1.position, -transform.up, out hit, GroundRayLenght, ground))
         {
             isGrounded = true;
             transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
@@ -68,16 +65,14 @@ public class CarController : MonoBehaviour
 
         if (isGrounded)
         {
-            _Rigidbody.drag = DragOnGround;
-            if (Mathf.Abs(SpeedInput) > 0)
-                _Rigidbody.AddForce(transform.forward * SpeedInput);
+            rigidBody.drag = dragOnGround;
+            if (Mathf.Abs(speedInput) > 0)
+                rigidBody.AddForce(transform.forward * speedInput);
         }
         else
         {
-            _Rigidbody.drag = 0.1f;
-            _Rigidbody.AddForce(Vector3.up * -gravityforce * 100f);
+            rigidBody.drag = 0.1f;
+            rigidBody.AddForce(Vector3.up * -gravityforce * 100f);
         }
-
-
     }
 }
